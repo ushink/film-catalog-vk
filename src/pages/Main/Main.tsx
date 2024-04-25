992062c719feac1060abb1d84ba0a6be90484c7b
Ulyana <ushinkevich@mail.ru>
2024-04-25 16:03:39 +0400

    main page layout

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,11 +1,20 @@
+import { Link } from "react-router-dom";
 import { Header } from "../../components/Header/Header";
 import s from "./Main.module.css";
+import { films } from "../../mock/filmsData";
 
 export function Main() {
   return (
     <>
       <Header />
-      <div className={s.main}>Main</div>
+      <main className={s.main}>
+        {films.map((el) => (
+          <Link to={`film/${el.id}`} key={el.id} className={s.container}>
+            <div className={s.img}></div>
+            <span className={s.title}>{el.name}</span>
+          </Link>
+        ))}
+      </main>
     </>
   );
 }
