ae5b175e4352abf8504606346237bec634e40e49
Ulyana <ushinkevich@mail.ru>
2024-04-26 15:21:54 +0400

    models ts

---
+++ src/models/models.ts
@@ -0,0 +1,21 @@
+export interface IMovies {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: any;
+  id: number;
+  media_type: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface ServerResponse<T> {
+  results: T[];
+}
