f6d511d25ff566443b5dd7bad97779e2f169734e
Ulyana <ushinkevich@mail.ru>
2024-04-26 15:22:34 +0400

    type store

--- src/store/slices/moviesSlice.ts
+++ src/store/slices/moviesSlice.ts
@@ -1,16 +1,25 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { IMovies } from "../../models/models";
+
+interface MoviesState {
+  movies: IMovies[];
+}
+
+const initialState: MoviesState = {
+  movies: [],
+};
 
 export const moviesSlice = createSlice({
   name: "movies",
-  initialState: {
-    movies: [],
-  },
+  initialState,
 
   reducers: {
-    setMovies: (state, action) => {
+    setMovies: (state, action: PayloadAction<IMovies[]>) => {
       state.movies = action.payload;
     },
   },
 });
+
+export const { setMovies } = moviesSlice.actions;
 
 export default moviesSlice.reducer;
